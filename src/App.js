29c2b2c5cc09ffded5db1cea67a19d21a5eb61a4
MatiasCalvi <alejandropreuss30@gmail.com>
2022-11-04 20:59:04 -0300

    fix-mt-f2-04

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home1 />}/>
+      {/* <Route path="/" element={<Home1 />}/> */}
       <Route path="/" element={<Home2 />}/>
     </Routes>
   
