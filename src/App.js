1d59bcbbe06079533ebbd3a099c6b6a97fd65377
nehuen-aumedes-diez <nehuen.aumedes@gmail.com>
2022-11-05 01:01:41 -0300

    mt-f1-05

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
-import Home1 from "./pages/Home1";
-import Home2 from "./pages/Home2";
-import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./layout/layout";
+
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home1 />}/>
-      <Route path="/" element={<Home2 />}/>
-    </Routes>
-  
+      
+    <Layout>
+
+    <Home />
+
+    </Layout>
+
     );
 }
 
--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { Link as NavLink } from "react-router-dom";
-{}
+
 export default function CallToAction(props) {
   let { page, rute } = props;
   console.log(rute);
