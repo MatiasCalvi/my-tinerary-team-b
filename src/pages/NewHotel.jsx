2a6b18b509ed34e400f760dcfe7fdfb853b91bc4
MatiasCalvi <alejandropreuss30@gmail.com>
2022-11-22 07:41:27 -0300

    fix-css

--- src/pages/NewHotel.jsx
+++ src/pages/NewHotel.jsx
@@ -34,20 +34,20 @@ export default function NewHotel() {
     }
 
     return (
-    <div className='flex j-center a-center total bg'>
+    <div className='flex j-center a-center total bg formContainer-newHotels'>
         <div className='flex P80 j-center a-center column'>
         <h1>Whant to add a new hotel?</h1>
 
-        <form className='flex column'>
+        <form className='flex column m20'>
             <div className='flex column g-10 '>
-                <Input ref={hotelNameImputElement} type='text' id='hotelName' placeholder='Hotel Name:'/>
-                <Input ref={cityIdInputElement} type='text' id='cityName' placeholder='City Name:'/>
-                <Input ref={photoImputElement} type='text' id='photo' placeholder='photo url:'/>
-                <Input ref={capacityInputElement} type='text' id='Capacity' placeholder='Capacity:'/>
-                <Input ref={userIdImputElement} type='text' name='admin' value='' placeholder='admin code'/>
+                <Input ref={hotelNameImputElement}  type='text' id='hotelName' placeholder='Hotel Name:'/>
+                <Input ref={cityIdInputElement}  type='text' id='cityName' placeholder='City Name:'/>
+                <Input ref={photoImputElement}  type='text' id='photo' placeholder='photo url:'/>
+                <Input ref={capacityInputElement}  type='text' id='Capacity' placeholder='Capacity:'/>
+                <Input ref={userIdImputElement}  type='text' name='admin' value='' placeholder='admin code'/>
                 <div className='flex j-between'>
-                    <input className='w-50 fs-2' type="reset" value="Clear Form" />
-                    <input className='w-50 fs-2' onClick={handleCreateHotel} type="submit" value="Submit" />
+                    <input className='w-50 fs-2 input-form-newHotel' type="reset" value="Clear Form" />
+                    <input className='w-50 fs-2 input-form-newHotel' onClick={handleCreateHotel} type="submit" value="Submit" />
                 </div>
             </div>
         </form>
